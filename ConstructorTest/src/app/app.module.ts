e724c45ad801016eea36e2aece421b14a6f496bb
dannychou <danny75@gmail.com>
2019-11-05 17:14:52 +0800

    增加Dashboard

--- ConstructorTest/src/app/app.module.ts
+++ ConstructorTest/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { MessageComponent } from './message/message.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    MessageComponent
+    MessageComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
